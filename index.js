eb9f8252ca8ad8a6d0775051e3a62a1e1eb3ab58
yixiangyin <y1027020239@qq.com>
2025-08-03 13:15:46 +1000

    3.13-3.14

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 var morgan = require("morgan");
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
+morgan.token("data", (req, res) => JSON.stringify(req.body));
 
-morgan.token('data', (req, res) => JSON.stringify(req.body));
-
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.post("/api/persons", (req, res) => {
@@ -44,25 +27,21 @@ app.post("/api/persons", (req, res) => {
       error: "The name or number is missing",
     });
   }
-  if (persons.some((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (persons.some((p) => p.name === body.name)) {
+  //   return res.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
-
-const generateId = () => {
-  return String(Math.ceil(Math.random() * 10000));
-};
 
 app.get("/info", (req, res) => {
   const time = new Date();
@@ -72,13 +51,9 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
