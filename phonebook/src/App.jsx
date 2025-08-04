71c93052378bc95bb526bbac7b5afec913511bf6
yixiangyin <y1027020239@qq.com>
2025-08-04 21:48:10 +1000

    error handling for frontend

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,98 +1,101 @@
-import { useState } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
-import { useEffect } from "react";
-import personService from "./services/persons";
-import Notification from "./components/Notification";
+import { useState } from "react"
+import Filter from "./components/Filter"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import { useEffect } from "react"
+import personService from "./services/persons"
+import Notification from "./components/Notification"
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
-  const [notification, setNotification] = useState({ message: null });
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
+  const [notification, setNotification] = useState({ message: null })
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
-      setPersons(initialPersons);
-    });
-  }, []);
+      setPersons(initialPersons)
+    })
+  }, [])
 
   const removePerson = (person) => {
-    const ok = window.confirm(`Delete ${person.name} ?`);
+    const ok = window.confirm(`Delete ${person.name} ?`)
     if (ok) {
       personService.remove(person.id).then(() => {
-        setPersons(persons.filter((p) => p.id !== person.id));
-        notifyWith(`Deleted ${person.name}`);
-      });
+        setPersons(persons.filter((p) => p.id !== person.id))
+        notifyWith(`Deleted ${person.name}`)
+      })
     }
-  };
+  }
 
   const notifyWith = (message, isError = false) => {
-    setNotification({ message, isError });
+    setNotification({ message, isError })
     setTimeout(() => {
-      setNotification({ message: null });
-    }, 5000);
-  };
+      setNotification({ message: null })
+    }, 5000)
+  }
 
   const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  )
 
   const clearForm = () => {
-    setNewName("");
-    setNewNumber("");
-  };
+    setNewName("")
+    setNewNumber("")
+  }
 
   const updatePerson = (person) => {
     const ok = window.confirm(
       `${newName} is already added to phonebook, replace the old number with a new one?`
-    );
+    )
     if (ok) {
       personService
         .update({ ...person, number: newNumber })
         .then((updatedPerson) => {
           setPersons(
             persons.map((p) => (person.id === p.id ? updatedPerson : p))
-          );
-          notifyWith(`Changed ${person.name}'s number`);
-          clearForm();
+          )
+          notifyWith(`Changed ${person.name}'s number`)
+          clearForm()
         })
         .catch((error) => {
-          notifyWith(`${error.response.data.error}`, true);
-          // notifyWith(
-          //   `Information of ${person.name} has already been removed from server`,
-          //   true
-          // );
-          // setPersons(persons.filter((p) => p.name !== person.name));
-        });
+          if (error.status === 400) {
+            notifyWith(error.response.data.error, true)
+          } else if (error.status === 404) {
+            notifyWith(
+              `Information of ${person.name} has already been removed from server`,
+              true
+            )
+            setPersons(persons.filter((p) => p.name !== person.name))
+          }
+        })
     }
-  };
+  }
 
   const addPerson = (event) => {
-    event.preventDefault();
-    const existingPerson = persons.find((person) => person.name === newName);
+    event.preventDefault()
+    const existingPerson = persons.find((person) => person.name === newName)
 
     if (existingPerson) {
-      updatePerson(existingPerson);
-      return;
+      updatePerson(existingPerson)
+      return
     }
     const personObject = {
       name: newName,
       number: newNumber,
-    };
+    }
     personService
       .create(personObject)
       .then((createdPerson) => {
-        setPersons(persons.concat(createdPerson));
-        notifyWith(`Added ${createdPerson.name}`);
-        clearForm();
+        setPersons(persons.concat(createdPerson))
+        notifyWith(`Added ${createdPerson.name}`)
+        clearForm()
       })
       .catch((error) => {
-        notifyWith(`${error.response.data.error}`, true);
-      });
-  };
+        notifyWith(error.response.data.error, true)
+      })
+  }
 
   return (
     <div>
@@ -114,7 +117,7 @@ const App = () => {
       {personsToShow.length === 0 && <p>No matching entries</p>}
       <Persons personsToShow={personsToShow} removePerson={removePerson} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
